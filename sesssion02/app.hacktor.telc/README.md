1cc0612775df04022e619259a7d35699baf4a8d0
majva majva <topcodermc@gmail.com>
2021-05-08 22:14:12 +0430

    second session

---
+++ sesssion02/app.hacktor.telc/README.md
@@ -0,0 +1,8 @@
+
+src:
+ - commons {this will contains all commons file here e.g: colors, constants, enums, ...}
+ - core { plugins, ioc, service_concentrate, ... }
+ - extentions { middelware }
+ - interfaces
+ - modules { service, data, ... }
+ - utils { decreators, read config, ... }
---
+++ sesssion02/app.hacktor.telc/src/core/startup.py
@@ -0,0 +1,14 @@
+"""
+    created by mjghasempour at 05/08/2021
+    email: topcodermc@gmail.com
+      -- this package will start up the whole of application 
+"""
+
+
+class StartUp:
+
+    def __init__(self):
+        super(StartUp).__init__(self)
+
+    def start(self):
+        print ("... Application start ...")
